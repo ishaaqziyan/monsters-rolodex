589a617919b8bd6f72370aef4980b49474cd8a5e
Ishaaq <ishaaqziyan@gmail.com>
2023-10-13 23:33:13 +0530

    Continued Development

--- src/App.js
+++ src/App.js
@@ -6,28 +6,32 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [{
-        name: 'Linda'
-      },
-      {
-        name: 'Frank'
-      },
-      {
-        name: 'Jacky'
-      },
-      {
-        name: 'Henry'
-      },
-    ]  
+      monsters: [],
     };
+    console.log('1');
   }
+
+  componentDidMount(){
+    console.log('3');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users};
+    },
+     () => {
+      console.log(this.state);
+     }
+    )
+    );
+  }
+
   render() {
+    console.log('2');
     return <div className="App">
       {
         this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>
-        })
-      }
+          return <div key = {monster.id}><h1>{monster.name}</h1></div>;
+        })}
     </div>;
   }
 }
