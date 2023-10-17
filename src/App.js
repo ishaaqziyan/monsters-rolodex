34c9ab22b8fa5c9e68167c6b11ba1d53ed17f868
Ishaaq <ishaaqziyan@gmail.com>
2023-10-17 12:38:13 +0530

    Deployed search-box component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,48 @@
 import {Component} from 'react';
-import logo from './logo.svg';
+import CardList from './Components/card-list/card-list.cmponent';
 import './App.css';
+import SearchBox from './Components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
     };
-    console.log('constructor');
   }
 
   componentDidMount(){
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then((users) => this.setState(() => {
       return {monsters: users};
-    },
-     () => {
-      console.log(this.state);
-     }
-    )
+    })
     );
   }
 
   onSearchChange = (event) => {
       const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(
-      () => {
+    this.setState(() => {
       return {searchField};
       });
-  }
+  };
 
   render() {
-    console.log('render');
-
+    //console.log('render from Appjs');
     const {monsters, searchField} = this.state;
     const {onSearchChange} = this;
 
     const filteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
-    return <div className="App">
-      <input 
-        className='search-box' 
-        type='search' 
-        placeholder='search monsters'
-        onChange={onSearchChange} 
-      />
-      {
-        filteredMonsters.map((monster) => {
-          return (
-            <div key = {monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
-    </div>;
+
+    return (
+    <div className='App'>
+      <SearchBox className='search-box' onChangeHandler = {onSearchChange} placeholder= {'search monsters'} />
+      <CardList monsters = {filteredMonsters}/>
+    </div>
+    );
   }
 }
 
